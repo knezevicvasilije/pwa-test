5d626f525590a495e20b17e3aeb654206c913f76
vaskodegama1 <vasilije.knezevic1@gmail.com>
2021-04-05 09:43:17 +0200

    git init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 if("serviceWorker" in navigator){
 navigator.serviceWorker.register("sw.js").then(registration => {
-    console.log("SW Registered")
+    console.log("SW Registeredd")
     console.log(registration)
 }).catch(error => {
     console.log("SW Registration Failed")
